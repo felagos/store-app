b806e0308897aa41d5c46e136b4e534a4fa3e6da
Felipe Lagos <felagossanchez@gmail.com>
2021-05-16 21:03:14 -0400

    set prefix api

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { ENV } from './environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api");
   await app.listen(ENV.PORT);
 }
 bootstrap();
