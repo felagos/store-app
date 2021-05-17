ab0e8b07c38b3e733835b1eb340cebfa4cf85b8a
Felipe Lagos <felagossanchez@gmail.com>
2021-05-16 21:24:32 -0400

    adding validations

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ENV } from './environment';
@@ -5,6 +6,12 @@ import { ENV } from './environment';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(ENV.PORT);
 }
 bootstrap();
--- server/src/product/dto/product.dto.ts
+++ server/src/product/dto/product.dto.ts
@@ -1,15 +1,26 @@
+import { PartialType } from "@nestjs/mapped-types";
+import { IsNotEmpty, IsNumber, IsString, IsUrl } from "class-validator";
+
 export class CreateProductDTO {
+    @IsString()
+    @IsNotEmpty()
     readonly name: string;
+
+    @IsString()
+    @IsNotEmpty()
     readonly description: string;
+
+    @IsNumber()
+    @IsNotEmpty()
     readonly price: number;
+
+    @IsNumber()
+    @IsNotEmpty()
     readonly stock: number;
+
+    @IsUrl()
+    @IsNotEmpty()
     readonly image: string;
 }
 
-export class UpdateProductDTO {
-    name?: string;
-    description?: string;
-    price?: number;
-    stock?: number;
-    image?: string;
-}
+export class UpdateProductDTO extends PartialType(CreateProductDTO) {}
