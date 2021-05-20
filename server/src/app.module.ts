b035501a6d4dcf13c55686e7a1e4422f4bfce553
Felipe Lagos <felagossanchez@gmail.com>
2021-05-19 22:12:27 -0400

    add user module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProductModule, CategoryModule],
+  imports: [ProductModule, CategoryModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
