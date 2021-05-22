3a2d2a88e94401283ab8df99d904c2f71bf5ad22
Felipe Lagos <felagossanchez@gmail.com>
2021-05-21 20:30:57 -0400

    enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -25,6 +25,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  app.enableCors();
+
   console.log(`PORT: ${port}`);
   await app.listen(port);
 }
