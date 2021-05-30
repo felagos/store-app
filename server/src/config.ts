356cbb52c4a5c0f63f6c54c772054e2ea250c0e5
Felipe Lagos <felagossanchez@gmail.com>
2021-05-30 13:18:32 -0400

    update package.json

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,6 +2,13 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs('config', () => {
     return {
-        port: process.env.APP_PORT
+        port: process.env.APP_PORT,
+        database: {
+            name: process.env.DB_NAME,
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+        }
     }
 });
--- server/src/schemas.ts
+++ server/src/schemas.ts
@@ -1,5 +1,10 @@
 import * as Joi from "joi";
 
 export const envSchema = Joi.object({
-    APP_PORT: Joi.number().required()
+    APP_PORT: Joi.number().required(),
+    DB_NAME: Joi.string().required(),
+    DB_USER: Joi.string().required(),
+    DB_PASSWORD: Joi.string().required(),
+    DB_HOST: Joi.string().required(),
+    DB_PORT: Joi.number().required()
 });
